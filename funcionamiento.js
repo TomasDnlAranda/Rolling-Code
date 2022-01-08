db2024590a1d7c942faab81d8df6b50e149d1e67
Chemmardo <mishuevos@gmail.com>
2022-01-08 12:20:13 -0300

    version 1

---
+++ funcionamiento.js
@@ -0,0 +1,9 @@
+const header = document.querySelector("body > div > header")
+const body = document.querySelector("body")
+
+window.addEventListener("scroll", () => {
+    header.style.background = "#ffffff"
+    header.style.transition = "background 1s"
+}
+)
+
